b1e0a70e1416955c2e6804c655aa5cbfadb2745e
Benjamin <benjamin.gayaud@epitech.eu>
2023-03-01 22:13:09 +0100

    [UPDATE] algo bronze to silver

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 struct player_s
 {
@@ -37,20 +38,24 @@ int main(void)
     struct checkpoint_s checkpoint = {0, 0, 0, 0};
     struct opponent_s opponent = {0, 0};
 
+    int distance = 0;
+    bool boost = false;
+
     while (1) {
         scanf("%d%d%d%d%d%d", &player.x, &player.y, &checkpoint.x, &checkpoint.y, &checkpoint.distance, &checkpoint.angle);
         scanf("%d%d", &opponent.x, &opponent.y);
 
-        if (checkpoint.angle > -45 && checkpoint.angle < 45 && checkpoint.distance > 500) {
-            player.thrust = 100;
+        if (checkpoint.angle > 90 || checkpoint.angle < -90) {
+            player.thrust = 0;
         } else {
-            player.thrust = 20;
+            player.thrust = 100;
         }
 
         printf("%d %d", checkpoint.x, checkpoint.y);
 
-        if (checkpoint.angle == 0 && checkpoint.distance >= 3000) {
+        if (checkpoint.angle == 0 && checkpoint.distance >= 5000 && boost == false) {
             printf(" BOOST\n");
+            boost = true;
         } else {
             printf(" %d\n", player.thrust);
         }
